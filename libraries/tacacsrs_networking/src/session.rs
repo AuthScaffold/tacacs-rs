b35d4594fe5c584921421b46cbccaaae4518c451
Rodney Persky <rodney.persky@outlook.com>
2024-09-22 18:31:31 +1000

    Add session testing

--- libraries/tacacsrs_networking/src/session.rs
+++ libraries/tacacsrs_networking/src/session.rs
@@ -48,7 +48,84 @@ impl Session
 
     pub async fn is_complete(&self) -> bool
     {
+        if self.duplex_channel.sender_closed().await
+        {
+            return true;
+        }
+
+        if self.duplex_channel.receiver_closed().await
+        {
+            return true;
+        }
+
         let session_complete_lock = self.session_complete.read().await;
         *session_complete_lock
+    }
+}
+
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+    use crate::duplex_channel::DuplexChannel;
+    use tacacsrs_messages::packet::Packet;
+    use tokio::sync::mpsc;
+
+    #[tokio::test]
+    async fn test_session()
+    {
+        let (network_sender, _network_receiver) = mpsc::channel::<Packet>(32);
+        let (_client_sender, client_receiver) = mpsc::channel::<Packet>(32);
+        let duplex_channel = DuplexChannel::new(client_receiver, network_sender);
+
+        let session = Session::new(1, duplex_channel);
+
+        assert_eq!(session.session_id(), 1);
+        assert_eq!(session.is_complete().await, false);
+
+        session.complete().await;
+
+        assert_eq!(session.is_complete().await, true);
+    }
+
+    #[tokio::test]
+    async fn test_sequence_number()
+    {
+        let (network_sender, _network_receiver) = mpsc::channel::<Packet>(32);
+        let (_client_sender, client_receiver) = mpsc::channel::<Packet>(32);
+        let duplex_channel = DuplexChannel::new(client_receiver, network_sender);
+
+        let session = Session::new(1, duplex_channel);
+
+        assert_eq!(session.next_sequence_number().await, 1);
+        assert_eq!(session.next_sequence_number().await, 3);
+        assert_eq!(session.next_sequence_number().await, 5);
+
+        assert_eq!(session.is_complete().await, false);
+    }
+
+    #[tokio::test]
+    async fn test_is_complete_network_closed()
+    {
+        let (network_sender, _network_receiver) = mpsc::channel::<Packet>(32);
+        let (_client_sender, client_receiver) = mpsc::channel::<Packet>(32);
+        let duplex_channel = DuplexChannel::new(client_receiver, network_sender);
+
+        let session = Session::new(1, duplex_channel);
+        assert_eq!(session.is_complete().await, false);
+
+        // Close the client sender, this should propagate to the session
+        drop(_client_sender);
+
+        // session is complete because the client sender is closed
+        assert_eq!(session.is_complete().await, true);
+
+        // the client sender is still open because it'll be used by many sessions
+        assert_eq!(_network_receiver.is_closed(), false);
+        assert_eq!(session.duplex_channel.sender_closed().await, false);
+
+        // the client receiver is closed
+        assert_eq!(session.duplex_channel.receiver_closed().await, true);
     }
 }
