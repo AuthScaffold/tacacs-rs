3e7388e52d3960726f2a581ed92a9661c01ba284
Rod Persky <rodpersky@microsoft.com>
2026-03-19 16:03:19 +1000

    remove converter

--- lde/convert-ipaddress.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [string]$ip
-)
-
-$bytes = [System.Net.IPAddress]::Parse($ip).GetAddressBytes()
-[Array]::Reverse($bytes)  # Ensure correct byte order
-[uint32]([BitConverter]::ToUInt32($bytes, 0))
